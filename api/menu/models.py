5fb536cf9f1367cea28164678c8e66ee41e25e6d
Blazej Bielski <bbielski@lyck.pl>
2023-07-02 13:47:30 +0200

    category model

--- api/menu/models.py
+++ api/menu/models.py
@@ -8,7 +8,10 @@ class ProductImage(models.Model):
 
 
 class Category(TimeStampModel):
-    pass
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return str(self.name)
 
 
 class Dish(TimeStampModel):
