e149be4229736dc4f2d21f441ff805fc58a5cc83
Blazej Bielski <bbielski@lyck.pl>
2023-07-02 13:45:46 +0200

    menu app with models dish and images, timestamp model utilities

---
+++ api/menu/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from utilities.timestamp_model import TimeStampModel
+
+
+class ProductImage(models.Model):
+    image = models.ImageField(upload_to="product_image")
+    product = models.ForeignKey("Dish", on_delete=models.CASCADE, related_name="images")
+
+
+class Category(TimeStampModel):
+    pass
+
+
+class Dish(TimeStampModel):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=300)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    category = models.ForeignKey("Category", on_delete=models.DO_NOTHING, related_name="categories")
+
+    def __str__(self):
+        return str(self.name)
