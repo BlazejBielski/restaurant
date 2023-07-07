315cb28d2e8e9c4096fe5bc03ffd02338457da33
Blazej Bielski <bbielski@lyck.pl>
2023-07-07 12:18:44 +0200

    add viewsets to menu

--- api/menu/urls.py
+++ api/menu/urls.py
@@ -1,9 +1,11 @@
+from . import viewsets
 from rest_framework.routers import DefaultRouter
 
 app_name = 'menu'
 
 router = DefaultRouter()
-
+router.register(r'categories', viewsets.CategoryViewSet, basename='category-view-set')
+router.register(r'dishes', viewsets.DishViewSet, basename='dish-view-set')
 
 urlpatterns = [
     *router.urls
---
+++ api/menu/viewsets.py
@@ -0,0 +1,77 @@
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from . import models
+from . import serializers
+
+
+class CategoryViewSet(ModelViewSet):
+    queryset = models.Category.objects.all()
+    serializer_class = serializers.CategorySerializer
+
+    def list(self, request, *args, **kwargs):
+        serializer = self.serializer_class(self.get_queryset(), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, *args, pk=None, **kwargs):
+        product = self._get_product(pk)
+        serializer = self.serializer_class(product)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, *args, pk=None, **kwargs):
+        product = self._get_product(pk)
+        serializer = self.serializer_class(instance=product, data=request.data, partial=kwargs.get('partial', False))
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def partial_update(self, request, *args, pk=None, **kwargs):
+        return self.update(request, *args, pk=pk, partial=True, **kwargs)
+
+    def destroy(self, request, *args, pk=None, **kwargs):
+        product = self._get_category(pk)
+        product.delete()
+        return Response(status=status.HTTP_200_OK)
+
+    def _get_category(self, pk):
+        return get_object_or_404(queryset=self.get_queryset(), pk=pk)
+
+
+class DishViewSet(ModelViewSet):
+    queryset = models.Dish.objects.all()
+    serializer_class = serializers.DishSerializer
+
+    def list(self, request, *args, **kwargs):
+        serializer = self.serializer_class(self.get_queryset(), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, *args, pk=None, **kwargs):
+        product = self._get_dish(pk)
+        serializer = self.serializer_class(product)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, *args, pk=None, **kwargs):
+        pass
+
+    def partial_update(self, request, *args, pk=None, **kwargs):
+        return self.update(request, *args, pk=pk, partial=True, **kwargs)
+
+    def destroy(self, request, *args, pk=None, **kwargs):
+        pass
+
+    def _get_dish(self, pk):
+        return get_object_or_404(queryset=self.get_queryset(), pk=pk)
